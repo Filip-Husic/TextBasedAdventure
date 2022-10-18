c94274a18a32823607374785875f4073135166b5
Matija Cvetan <matija.cvetan@awacademy.training>
2022-10-18 14:30:41 +0200

    chest + minor fix to FirstMap

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,10 @@
-import game_interface.Menu;
-import map.FirstFloor;
-
-import java.util.Scanner;
+import map.FirstMap;
 
 @SuppressWarnings({"BusyWait", "unused"})
 public class Main {
     public static void main(String[] args) throws InterruptedException {
-        FirstFloor firstFloor = new FirstFloor();
+        FirstMap firstFloor = new FirstMap();
 
-        firstFloor.setPlayerOnMap();
         firstFloor.showMap();
         firstFloor.move();
 

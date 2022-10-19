a1f08030b9e136725555682f2e4339d49ef9b8f4
Filip-Husic <105728416+Filip-Husic@users.noreply.github.com>
2022-10-19 17:53:31 +0200

    reverted main to original

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,10 @@
-import battle.Battle;
 import game_interface.Game;
-import monster.Skeleton;
-import player.*;
 
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
-//        Game game = new Game();
-//        game.intro();
-
-        Player player = new Warrior();
-        Skeleton skeleton = new Skeleton(2);
-        Battle battle = new Battle();
-        battle.battleStart(player,skeleton);
+        Game game = new Game();
+        game.intro();
 
     }
 
